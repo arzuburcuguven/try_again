e381db58dd2319c0006fdd04e1a9508ab55e26ca
Arzu Burcu Guven <arzuburcuguven@gmail.com>
2025-01-13 16:57:02 +0100

    hhh

--- tests/test_training.py
+++
@@ -1,9 +0,0 @@
-import torch
-from src.mnist.train import train
-from src.mnist.data import MyDataset
-import os
-
-def test_my_training():
-    train(1e-3, 32, 10)
-    assert os.path.exists(os.path.abspath("/Users/argy/workspace/try_again/models/model.pth"))
-
